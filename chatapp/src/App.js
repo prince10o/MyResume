5cad8ca0e3248e4fa4cd5b4374b4ade2de5293b7
unknown <amritpreet165@gmail.com>
2024-05-06 12:39:52 +0530

    first commit

---
+++ chatapp/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import ChatApp from "./components/ChatApp";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <ChatApp />
+    </div>
+  );
+}
+
+export default App;
